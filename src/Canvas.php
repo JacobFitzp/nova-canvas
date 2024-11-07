7f2821bb6f36c252d77ee6a5516006cbf06be77c
Tarvo R. <TarvoReinpalu@gmail.com>
2024-11-07 12:51:19 +0200

    Add show source command and button

--- src/Canvas.php
+++ src/Canvas.php
@@ -3,9 +3,12 @@
 namespace Jacobfitzp\NovaCanvas;
 
 use Laravel\Nova\Fields\Field;
+use Laravel\Nova\Fields\SupportsDependentFields;
 
 class Canvas extends Field
 {
+    use SupportsDependentFields;
+
     /**
      * The field's component.
      *
